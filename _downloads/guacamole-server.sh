cedbfcbaf9ac7c6ec88e78bcfaf5a63433cc4316
Gerald Lovel <glovel@aaltsys.com>
2016-02-15 07:27:55 -0600

    Deployed documentation

--- _downloads/guacamole-server.sh
+++ _downloads/guacamole-server.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# one-time program to setup Guacamole Server on Ubuntu
+# one-time program to setup Guacamole Server and Client on Ubuntu
 
-echo "Installing Guacamole Server on Ubuntu"
+echo "Installing Guacamole Server and Client on Ubuntu"
 
-# if [[ $EUID -ne 0 ]] ; then echo -e "\e[1;31m try again using sudo \e[0m" ; exit 1 ; fi
-
-sudo bash << SERVER
+sudo bash << SCRIPT
 
    # install dependencies
-   apt-get install libcairo2-dev libjpeg62-dev libpng12-dev libossp-uuid-dev 
-   apt-get install libfreerdp-dev libpango1.0-dev libssh2-1-dev libssh-dev 
-   apt-get install tomcat7 tomcat7-admin tomcat7-user 
+   apt-get -y install libcairo2-dev libjpeg62-dev libpng12-dev libossp-uuid-dev 
+   apt-get -y install libfreerdp-dev libpango1.0-dev libssh2-1-dev libssh-dev 
+   apt-get -y install tomcat7 tomcat7-admin tomcat7-user 
 
    # download and extract Guacamole tarball
    wget http://sourceforge.net/projects/guacamole/files/current/source/guacamole-server-0.9.9.tar.gz 
@@ -24,25 +22,20 @@ sudo bash << SERVER
    make install
    ldconfig
 
-SERVER
+   # download and rename client
+   cd /var/lib/tomcat7
+   wget http://sourceforge.net/projects/guacamole/files/current/binary/guacamole-0.9.9.war
+   mv guacamole-0.9.9.war guacamole.war
 
-# one-time program to setup Guacamole Client on Ubuntu
+   # create the configuration file
+   mkdir /etc/guacamole
+   mkdir /usr/share/tomcat7/.guacamole
 
-echo "Install Guacamole Client on Ubuntu"
-
-# download and rename client
-
-cd /var/lib/tomcat7
-wget http://sourceforge.net/projects/guacamole/files/current/binary/guacamole-0.9.9.war
-mv guacamole-0.9.9.war guacamole.war
-
-# create the configuration file
-
-mkdir /etc/guacamole
-mkdir /usr/share/tomcat7/.guacamole
+SCRIPT
 
 # write the configuration
-echo /etc/guacamole/guacamole.properties << CONF
+
+sudo echo /etc/guacamole/guacamole.properties << CONF
    guacd-hostname:      localhost
    guacd-port:          4822
    user-mapping:        /etc/guacamole/user-mapping.xml
@@ -51,6 +44,8 @@ echo /etc/guacamole/guacamole.properties << CONF
 CONF
 
 # create a symbolic link to configuration for tomcat:
-ln -s /etc/guacamole/guacamole.properties /usr/share/tomcat7/.guacamole/
+sudo ln -s /etc/guacamole/guacamole.properties /usr/share/tomcat7/.guacamole/
+
+echo "Finished installing guacamole server and client.||Now create the user-mapping.xml"
 
 exit
